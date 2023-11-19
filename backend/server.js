d61f680ac8e440609a9c4b1e5d4ef891576a95a2
= <=>
2023-11-19 22:36:31 +0530

    api routes and setup

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const { chats } = require("./data/data");
-const app = express();
 
-const PORT = 5000;
+const app = express();
+dotenv.config();
+// PORT
+const PORT = process.env.PORT || 5000;
 
 // ROUTES
 app.get("/", (req, res) => {
@@ -13,6 +16,13 @@ app.get("/api/chat", (req, res) => {
   res.send(chats);
 });
 
+app.get("/api/chat/:id", (req, res) => {
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  //   console.log(singleChat);
+  res.send(singleChat);
+});
+
+// LISTEN
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}🚀`);
 });
